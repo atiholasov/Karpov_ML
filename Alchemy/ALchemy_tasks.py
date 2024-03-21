8bd55275a572484db03dfb7b9a7826a2f9608c25
Alexey Tiholasov <alexey.tiholasov@apstecsystems.com>
2024-03-21 13:25:21 +0300

    table_post

--- Alchemy/ALchemy_tasks.py
+++ Alchemy/ALchemy_tasks.py
@@ -2,14 +2,10 @@ from sqlalchemy import Column, Integer, String, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# NOTE be careful when sharing
-# SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/database"
 SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
 
-# создаём engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# настройка класса Session c требуемыми настройками
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
