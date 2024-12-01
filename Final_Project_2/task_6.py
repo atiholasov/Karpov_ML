3febef79dec603f01117739afe2bfdceb0ca339d
atiholasov <alexey.tiholasov@apstecsystems.com>
2024-12-01 13:20:43 +0200

    task_6.py

--- Final_Project_2/task_6.py
+++ Final_Project_2/task_6.py
@@ -5,26 +5,25 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
-CHUNKSIZE = 1000
+CHUNKSIZE = 10000
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
-def get_data_from_db(url, chunksize):
-    engine = create_engine(url)
-    DATA = f"""SELECT user_id
-                FROM feed_action
-                LIMIT 10000
-                """
-    return pd.read_sql(DATA, engine, chunksize=chunksize)
-
-
-for DF_chunk in get_data_from_db(SQLALCHEMY_DATABASE_URL, CHUNKSIZE):
-
-    DF_chunk.to_sql('alexey_tiholasov_features_lesson_22', con=engine, if_exists='append', index=False)
+# def get_data_from_db(url, chunksize):
+#     engine = create_engine(url)
+#     DATA = f"""SELECT user_id
+#                 FROM feed_action
+#                 """
+#     return pd.read_sql(DATA, engine, chunksize=chunksize)
+#
+#
+# for DF_chunk in get_data_from_db(SQLALCHEMY_DATABASE_URL, CHUNKSIZE):
+#
+#     DF_chunk.to_sql('alexey_tiholasov_features_lesson_22', con=engine, if_exists='replace', index=False)
 
 
 def batch_load_sql(query: str) -> pd.DataFrame:
-    CHUNKSIZE = 1000
+    CHUNKSIZE = 20000
 
     SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
